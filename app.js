330b589d2a9e7a648d92b5749b3d12aebe287abc
Michael Prather <mprather1@gmail.com>
2016-05-13 21:40:39 -0500

    ng-repeat working

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-angular.module("flapperNews", [])
-.controller("MainCtrl", [
-  '$scope',
-  function($scope){
-    $scope.test = 'Hello';
-  }
-])
+angular.module('flapperNews', [])
+.controller('MainCtrl', [
+'$scope',
+function($scope){
+  $scope.posts = [
+    'post 1',
+    'post 2',
+    'post 3',
+    'post 4',
+    'post 5'
+  ];
+}]);
