b1232f9d22991106c64b91bfc1aa36b6458086f0
Michael Prather <mprather1@gmail.com>
2016-05-15 17:49:53 -0500

    factory

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 angular.module('flapperNews', [])
+.factory('posts', [function() {
+  var o = {
+    posts: []
+  };
+  return o;
+}])
 .controller('MainCtrl', [
 '$scope',
-function($scope){
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4},
-  ];
+'posts',
+function($scope, posts){
+  $scope.posts = posts.posts;
+  // $scope.posts = [
+  //   {title: 'post 1', upvotes: 5},
+  //   {title: 'post 2', upvotes: 2},
+  //   {title: 'post 3', upvotes: 15},
+  //   {title: 'post 4', upvotes: 9},
+  //   {title: 'post 5', upvotes: 4},
+  // ];
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === '') { return; }
     $scope.posts.push({
